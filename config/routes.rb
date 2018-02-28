a71efaf8194e6b38812259ecbdf820d1433e1a3b
Rails Udemy Course <webbc99@gmail.com>
2018-02-28 12:08:34 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 	get 'contact', to: 'pages#contact'
 
 	resources :articles
+
+	get 'signup', to: 'users#new'
+	resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
