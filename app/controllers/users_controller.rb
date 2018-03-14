a086ff51d8fe1ed43e3afb472eb56f955754125a
Rails Udemy Course <webbc99@gmail.com>
2018-03-14 20:25:03 +0000

    Updated all views with categories

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
 	end
 
 	def show
-		@user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
+		@user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
 	end
 
 	def destroy
